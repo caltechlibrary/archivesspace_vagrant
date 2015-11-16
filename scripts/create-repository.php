44c4c64986b21ac214a8625b412ea65fb259190e
R. S. Doiel <rsdoiel@gmail.com>
2015-11-16 15:42:47 -0800

    Create create-repository.php

---
+++ scripts/create-repository.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * create-repository.php - Create a development repository in Archivespace based on the environment described in setup.conf.
+ * This is a command line tool.
+ * @author R. S. Doiel, <rsdoiel@calteche.edu>
+ * copyright (c) 2015 all rights reserved
+ * Caltech Library
+ */
+
+
+?>
