a398ae25c3f18c0766d9a2b12b9dcf6e07d30c7b
R. S. Doiel <rsdoiel@gmail.com>
2015-11-16 15:44:04 -0800

    Create generate-test-data.php

---
+++ scripts/generate-test-data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * generate-test-data.php - Based on the environment described in setup.conf, contact the production deployment and request
+ * a data dump suitable for development purposes.
+ *
+ * This is a command line tool.
+ *
+ * @author R. S. Doiel, <rsdoiel@calteche.edu>
+ * copyright (c) 2015 all rights reserved
+ * Caltech Library
+ */
+?>
